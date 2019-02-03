7bdc1d230aceb30dbb84b77fd0b7d4ecd2e363ac
acarta <anthonym.cartagena@gmail.com>
2019-02-02 20:52:39 -0500

    Updates

---
+++ precache-manifest.a3ea7c69610474636787bcc50699f344.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "22b3854dd41c82ec05cd",
+    "url": "/Clicky-Game/static/js/main.22b3854d.chunk.js"
+  },
+  {
+    "revision": "5cabbdc29baec113d4ea",
+    "url": "/Clicky-Game/static/js/1.5cabbdc2.chunk.js"
+  },
+  {
+    "revision": "22b3854dd41c82ec05cd",
+    "url": "/Clicky-Game/static/css/main.52090473.chunk.css"
+  },
+  {
+    "revision": "b3f600b1ef82199506766b089c471e2b",
+    "url": "/Clicky-Game/index.html"
+  }
+];
